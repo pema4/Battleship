7c2839ea5adf53b4144fe0d167ff11c3e37c337c
pema4 <petelin.maksim@live.com>
2020-05-24 13:15:27 +0300

    final touches

--- ui/src/battleship/Main.java
+++ ui/src/battleship/Main.java
@@ -1,5 +1,7 @@
 package battleship;
 
+import battleship.controllers.PreparationController;
+import battleship.models.Role;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,12 +17,42 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/start.fxml"));
-        primaryStage.setTitle("Battleship");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
+    public void start(Stage stage) throws IOException {
+        var parameters = getParameters().getRaw();
+        Parent root;
+        if (parameters.size() == 1) {
+            var parameter = parameters.get(0).toLowerCase();
+            switch (parameter) {
+                case "server":
+                    stage.setResizable(true);
+                    stage.setMinWidth(370);
+                    stage.setMinHeight(535);
+                    root = loadPreparationScene(Role.SERVER);
+                    break;
+                case "client":
+                    stage.setResizable(true);
+                    stage.setMinWidth(370);
+                    stage.setMinHeight(535);
+                    root = loadPreparationScene(Role.CLIENT);
+                    break;
+                default:
+                    System.out.println("Wrong argument");
+                    return;
+            }
+        } else {
+            root = FXMLLoader.load(getClass().getResource("/fxml/start.fxml"));
+        }
 
-        primaryStage.show();
+        stage.setTitle("Battleship");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    private Parent loadPreparationScene(Role role) throws IOException {
+        var loader = new FXMLLoader(getClass().getResource("/fxml/preparation.fxml"));
+        loader.setControllerFactory(cls ->
+                new PreparationController(role));
+        return loader.load();
     }
 }
